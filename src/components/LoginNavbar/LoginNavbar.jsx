e2e25de4fcee01c4a407ed8fbbbb18bb08ab4c54
jose albin <josealbin@albins-MacBook-Pro.local>
2025-04-18 20:26:41 +1000

    admin panel progress

--- src/components/LoginNavbar/LoginNavbar.jsx
+++ src/components/LoginNavbar/LoginNavbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleInfo, faGear } from '@fortawesome/free-solid-svg-icons'
-import '../Navbar/Navbar.css'
+import '../LoginNavbar/LoginNavbar.css'
 import { Link } from 'react-router-dom'
 
 
