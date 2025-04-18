1a52f33d287fde9dd8b42a9eecfbcb8f147f4bb7
jose albin <josealbin@albins-MacBook-Pro.local>
2025-04-19 07:32:03 +1000

    css work continues

--- src/components/LoginNavbar/LoginNavbar.jsx
+++ src/components/LoginNavbar/LoginNavbar.jsx
@@ -14,7 +14,7 @@ function LoginNavbar() {
                     <Link to="/" className="logo-header">StockTaker.</Link>
                 </div>
                 <nav className='user_block'>
-                    <button><Link to="/admin-login"><FontAwesomeIcon icon={faGear} className='icon' /></Link></button> 
+                    <button><Link to="/admin-login"><FontAwesomeIcon icon={faGear} className='log-icon' /></Link></button> 
                     <button><Link to="/information"><FontAwesomeIcon icon={faCircleInfo} className='log-icon' /></Link></button>
                 </nav>
             </div>
