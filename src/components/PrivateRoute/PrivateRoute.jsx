b4b1251dfe89a92447c0dfeaea6523fbae85de24
Albin Jose <jose.albin@hotmail.com>
2025-06-09 16:01:22 +1000

    added protection to child routes such as /inventory

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
-function PrivateRoute({children}) {
-    const isAuthenticated = localStorage.getItem('username'); // Check if the username is stored in localStorage
-    return isAuthenticated ? children : <Navigate to="/" />;
-}
+function PrivateRoute({ children }) {
+    const isAuthenticated = !!localStorage.getItem('username');
+    if (isAuthenticated === null) return null; // optionally show loading
+    return isAuthenticated ? children : <Navigate to="/" replace />;
+  }
 
 export default PrivateRoute
