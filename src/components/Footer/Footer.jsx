9acafdf870dad484ff4328e4ca4fb9707ea0fa42
Albin Jose <jose.albin@hotmail.com>
2026-07-29 22:44:32 +1000

    footer brand removed temp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='container'>
-        <p>© ___ 2026 | All rights reserved.</p>
+        <p>© ____ 2026 | All rights reserved.</p>
       </div>
     </div>
   )
